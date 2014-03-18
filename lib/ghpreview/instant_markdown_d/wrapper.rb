1f5f4cc4405901af10250a3cadeddd23c8699ad3
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-03-18 13:47:50 +0900

    Change constant `STYLED_TEMPLATE_FILEPATH`
    /tmp/ghpreview.erb -> /tmp/ghpreview-instant_markdown_d-template.erb

--- lib/ghpreview/instant_markdown_d/wrapper.rb
+++ lib/ghpreview/instant_markdown_d/wrapper.rb
@@ -1,6 +1,9 @@
 require 'ghpreview/wrapper'
 
 GHPreview::Wrapper.class_eval do
+  remove_const :STYLED_TEMPLATE_FILEPATH
+  const_set :STYLED_TEMPLATE_FILEPATH, "/tmp/ghpreview-instant_makrdown_d-template.erb"
+
   remove_const :RAW_TEMPLATE_FILEPATH
   const_set :RAW_TEMPLATE_FILEPATH, "#{File.dirname(__FILE__)}/template.erb"
 end
