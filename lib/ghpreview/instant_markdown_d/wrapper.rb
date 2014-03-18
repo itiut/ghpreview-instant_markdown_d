5676da7facc912efffbc58da7d83b64b889d9bb0
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-03-18 15:09:49 +0900

    Change to single quotations

--- lib/ghpreview/instant_markdown_d/wrapper.rb
+++ lib/ghpreview/instant_markdown_d/wrapper.rb
@@ -2,7 +2,7 @@ require 'ghpreview/wrapper'
 
 GHPreview::Wrapper.class_eval do
   remove_const :STYLED_TEMPLATE_FILEPATH
-  const_set :STYLED_TEMPLATE_FILEPATH, "/tmp/ghpreview-instant_makrdown_d-template.erb"
+  const_set :STYLED_TEMPLATE_FILEPATH, '/tmp/ghpreview-instant_makrdown_d-template.erb'
 
   remove_const :RAW_TEMPLATE_FILEPATH
   const_set :RAW_TEMPLATE_FILEPATH, "#{File.dirname(__FILE__)}/template.erb"
