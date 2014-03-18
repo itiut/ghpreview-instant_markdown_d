580ce902a1edfd75327826198486572b86b75e6c
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-03-18 13:44:49 +0900

    Add `open` command for OS X

--- lib/ghpreview/instant_markdown_d/viewer.rb
+++ lib/ghpreview/instant_markdown_d/viewer.rb
@@ -1,11 +1,14 @@
 module GHPreview
   module InstantMarkdownD
     class Viewer
-
       def self.open_url(host, port)
-        `xdg-open http://#{host}:#{port}/`
+        command = if RUBY_PLATFORM =~ /linux/
+                    'xdg-open'
+                  else
+                    'open'
+                  end
+        `#{command} http://#{host}:#{port}`
       end
-
     end
   end
 end
